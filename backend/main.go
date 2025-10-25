61e59ed9589601a32c256b3f5fbea3bb81afaf55
f58dev <192527421+f58dev@users.noreply.github.com>
2025-10-25 23:43:21 +0300

    backend version1

---
+++ backend/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("🚀 Hello from Fiber!")
+	})
+
+	app.Listen(":5000")
+}
